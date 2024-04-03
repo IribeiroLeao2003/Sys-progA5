fbd565e8be15d28e2ad3b28482159629d5cd3e54
JakeWarywoda <jwarywoda7248@conestogac.on.ca>
2024-04-03 07:01:59 -0400

    spawnDC method added to spawn the DC in DC/bin/ and passing along the shmID, the PID of DP1 and PID of DP2

--- DP-2/inc/DP2.h
+++ DP-2/inc/DP2.h
@@ -1,3 +1,10 @@
 #include "../../common/inc/common.h"
 
-#define kPathToDCexe    "../DC/bin"
+#define kPathToDCexe    "../../DC/bin/dc"
+#define kDCProcessName  "dc"
+
+#define kshmIDBufferSize    10
+#define kPIDSize            10
+
+
+void spawnDC(int* shmID, pid_t DP1_pid, pid_t DP2_pid);
--- DP-2/src/DP2_Comm.c
+++ DP-2/src/DP2_Comm.c
@@ -1,1 +1,16 @@
 #include "DP2.h"
+
+void spawnDC(int* shmID, pid_t DP1_pid, pid_t DP2_pid) {
+    // Char buffers for passing when spawning DC
+    char shmID_string[kshmIDBufferSize];
+    char DP1_p[kPIDSize];
+    char DP2_p[kPIDSize];
+
+    // Fill the buffers
+    sprintf(shmID_string, "%d", shmID);
+    sprintf(DP1_p, "%d", DP1_pid);
+    sprintf(DP2_p, "%d", DP2_pid);
+
+    // Use execl with our 3 args to pass
+    execl(kPathToDCexe, kDCProcessName, shmID_string, DP1_p, DP2_p, (char *)NULL);
+}
